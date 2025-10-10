0bab214bbb516e4d02ae2abf7f68560c7c22ee6a
eby <ebykachappillil@gmail.com>
2025-10-10 23:55:09 +1100

    updation for handling openai api

--- Module 5 - Agents/test_openrouter.py
+++ Module 5 - Agents/test_openrouter.py
@@ -2,7 +2,7 @@ from openai import OpenAI
 
 client = OpenAI(
   base_url="https://openrouter.ai/api/v1",
-  api_key="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",
+  api_key="key_here",
 )
 
 completion = client.chat.completions.create(
